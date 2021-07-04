2c87fea3d529a1e4a7b3897fc90d41d6470bd077
tani <tani@1-10.com>
2021-07-04 16:31:22 +0900

    Update initialState reducers

--- src/reducks/users/reducers.js
+++ src/reducks/users/reducers.js
@@ -1,9 +1,14 @@
-const initialState = {
-  users: {
-    isSignedIn: false,
-    uid: '',
-    username: '',
-  },
-}
+import * as Actions from './actions'
+import initialState from '../store/initialState'
 
-export default initialState
+export const UsersReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case Actions.SIGN_IN:
+      return {
+        ...state,
+        ...action.payload,
+      }
+    default:
+      return state
+  }
+}
