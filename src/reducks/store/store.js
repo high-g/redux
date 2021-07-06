a7bf3d31b7f508f60aa3bdf93388a73614ff88dd
tani <tani@1-10.com>
2021-07-06 12:12:13 +0900

    Update

--- src/reducks/store/store.js
+++ src/reducks/store/store.js
@@ -2,7 +2,9 @@ import { createStore as reduxCreateStore, combineReducers } from 'redux'
 import { UsersReducer } from '../users/reducers'
 
 export default function createStore() {
+  // 返すものは、reduxのcreateStore（reduxCreateStoreという名称にしている）
   return reduxCreateStore(
+    // combineReducersはreducerを統合したもの
     combineReducers({
       users: UsersReducer,
     })
