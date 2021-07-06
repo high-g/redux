5d4094f35768e9413d1e1a6296150bdfcb1780d0
tani <tani@1-10.com>
2021-07-06 12:52:08 +0900

    Update redux index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
-import createStore from './reducks/'
+import createStore from './reducks/store/store'
 import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
