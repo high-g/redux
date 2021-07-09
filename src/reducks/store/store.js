74fd548abedcf6e5fcbf19ce02e43ca64e0c7675
tani <tani@1-10.com>
2021-07-09 13:38:56 +0900

    Update store

--- src/reducks/store/store.js
+++ src/reducks/store/store.js
@@ -1,15 +1,18 @@
-import { createStore as reduxCreateStore, combineReducers, applyMiddleware } from 'redux'
+import {
+  createStore as reduxCreateStore,
+  combineReducers,
+  applyMiddleware,
+} from 'redux'
 import { connectRouter, routerMiddleware } from 'connected-react-router'
 import { UsersReducer } from '../users/reducers'
+import thunk from 'redux-thunk'
 
 export default function createStore(history) {
-  // 返すものは、reduxのcreateStore（reduxCreateStoreという名称にしている）
   return reduxCreateStore(
-    // combineReducersはreducerを統合したもの
     combineReducers({
       router: connectRouter(history),
       users: UsersReducer,
     }),
-    applyMiddleware(routerMiddleware(history))
+    applyMiddleware(routerMiddleware(history), thunk),
   )
 }
